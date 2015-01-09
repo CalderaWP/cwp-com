ef57c1faa280ffe19166ec2456f27526e95aab79
Josh Pollock <jpollock412@gmail.com>
2015-01-09 13:31:46 -0500

    ad baldrick modal css

--- public_html/cwp-content/cwp-themes/cwp-theme/includes/baldrick.php
+++ public_html/cwp-content/cwp-themes/cwp-theme/includes/baldrick.php
@@ -14,6 +14,7 @@
  */
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_script( 'wp-balrick', get_stylesheet_directory_uri() ."/assets/js/src/wp-baldrick-full.js");
+	wp_enqueue_style( 'wp-balrick', get_stylesheet_directory_uri() ."/assets/css/baldrick/modal.css");
 });
 
 /**
