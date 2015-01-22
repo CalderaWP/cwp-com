359217860fe4d25f1bc10fe01dc60af6ae2d19e5
Josh Pollock <jpollock412@gmail.com>
2015-01-21 20:33:39 -0500

    Caldera Answers links

--- public/cwp-content/cwp-themes/cwp-theme/includes/CWP_Front_Page_Data.php
+++ public/cwp-content/cwp-themes/cwp-theme/includes/CWP_Front_Page_Data.php
@@ -48,14 +48,14 @@ class CWP_Front_Page_Data {
 			array(
 				'title' => 'Caldera Answers',
 				'tagline' => 'WordPress Development, The Right Way',
-				'title_link' => '#',
+				'title_link' => get_permalink( CWP_Theme_Caldera_Answers::$caldera_answers_id ),
 				'title_link_title' => 'Caldera Answers: WordPress Development, The Right Way',
 				'background' => 9,
 				'background_style' => '',
 				'content' => array(
 					'p' => array(
 						'Caldera Answers is a set of resources, and training tools for learning WordPress development the right way, by CalderaWP\'s Josh Pollock. Josh is contributor to the top WordPress news and tutorial sites, including <a href="http://jpwp.me/tutsplus">Tuts+</a>, <a href="http://torquemag.io/author/joshp/">Torque</a>, <a href="http://jpwp.me/wpmu">WPMUDEV</a>, <a href="http://jpwp.me/wpbeginner">WPBegginer</a>.',
-						sprintf( 'Through Caldera Answers he offers his highly-opinionated collection of %1s as well as training courses. The %2s teach WordPress development using a project-oriented approach designed to give beginner and intermediate WordPress developers practical training using establish standards, the best tools and best practices.', CWP_Theme_Caldera_Answers::caldera_answers_link( '"How To Do X With WordPress" links' ), CWP_Theme_Caldera_Answers::caldera_answers_course_link( 'Caldera Answers Courses' ) ),
+						sprintf( 'Through Caldera Answers he offers his highly-opinionated collection of %1s as well as training courses. The %2s teach WordPress development using a project-oriented approach designed to give beginner and intermediate WordPress developers practical training using establish standards, the best tools and best practices.', CWP_Theme_Caldera_Answers::search_page_link( '"How To Do X With WordPress" links' ), CWP_Theme_Caldera_Answers::course_page_link( 'Caldera Answers Courses' ) ),
 					)
 				),
 			)
--- public/cwp-content/cwp-themes/cwp-theme/includes/CWP_Theme_Caldera_Answers.php
+++ public/cwp-content/cwp-themes/cwp-theme/includes/CWP_Theme_Caldera_Answers.php
@@ -10,13 +10,84 @@
  */
 
 class CWP_Theme_Caldera_Answers {
+	/**
+	 * ID for Caldera Answers parent page
+	 *
+	 * @var int
+	 */
+	public static $caldera_answers_id = 283;
 
+	/**
+	 * ID for Caldera Answers search page
+	 *
+	 * @var int
+	 */
+	public static $caldera_answers_search_id = 400;
 
-	static function caldera_answers_link( $text ) {
-		return $text;
+	/**
+	 * ID for Caldera Answers courses page
+	 *
+	 * @var int
+	 */
+	public static $caldera_answers_courses_id = 398;
+
+	/**
+	 * Link for Caldera Answers parent page
+	 *
+	 * @param bool|string $text Optional. Text of link. Default is post title.
+	 *
+	 * @return string
+	 */
+	static function parent_page_link( $text = false) {
+		return self::link( self::$caldera_answers_id, $text );
+
 	}
 
-	public static function caldera_answers_course_link( $text ) {
-		return $text;
+	/**
+	 * Link for Caldera Answers search page
+	 *
+	 * @param bool|string $text Optional. Text of link. Default is post title.
+	 *
+	 * @return string
+	 */
+	static function search_page_link(  $text = false ) {
+		return self::link( self::$caldera_answers_search_id, $text );
+
+	}
+
+	/**
+	 * Link for Caldera Answers courses page
+	 *
+	 * @param bool|string $text Optional. Text of link. Default is post title.
+	 *
+	 * @return string
+	 */
+	public static function course_page_link( $text = false ) {
+		return self::link( self::$caldera_answers_courses_id, $text );
+
+	}
+
+	/**
+	 * Create links for Caldera Answers Pages
+	 *
+	 * @access protected
+	 *
+	 * @param int $id Post ID
+	 * @param bool|string $text Optional. Text of link. Default is post title.
+	 *
+	 * @return string
+	 */
+	protected static function link( $id, $text = false )  {
+		$post = get_post( $id );
+		$link = get_permalink( $id );
+
+		if ( ! is_string( $text ) ) {
+			$text = $post->post_title;
+		}
+
+		$link = sprintf( '<a href="%1s" title="%2s">%3s</a> ', $link, $post->post_title, $text );
+
+		return $link;
+
 	}
 }
