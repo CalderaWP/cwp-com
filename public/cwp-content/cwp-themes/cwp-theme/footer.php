cf66c61b2b2aabfb5693b057c9f6c8658571a177
Josh Pollock <jpollock412@gmail.com>
2015-02-02 18:50:27 -0500

    fix footer credit style, text

--- public/cwp-content/cwp-themes/cwp-theme/footer.php
+++ public/cwp-content/cwp-themes/cwp-theme/footer.php
@@ -27,8 +27,10 @@
 
 				<p class="credits-left">
 
-					&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
-
+					<span>
+						Site &copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="CalderaWP LLC">CalderaWP LLC</a>
+					</span>
+					<span> &mdash; <a href="https://github.com/CalderaWP/cwp-com/" title="Source Code For This Site">Source</a></span>
 				</p>
 
 				<p class="credits-right">
@@ -37,7 +39,7 @@
 						<?php printf( __( 'Theme by <a href="%s">Anders Noren</a>', 'hemingway'), 'http://www.andersnoren.se' ); ?>
 					</span> &mdash;
 					<span>
-						<?php printf( __( 'Child Theme by <a href="%s">Josh Pollock</a>', 'hemingway'), 'http://www.joshpress.net' ); ?>
+						<?php printf( __( 'Child Theme by <a href="%s">Josh Pollock</a>', 'cwp-com'), 'http://www.joshpress.net' ); ?>
 					</span> &mdash;
 					<a title="<?php _e('To the top', 'hemingway'); ?>" class="tothetop"><?php _e('Up', 'hemingway' ); ?> &uarr;</a>
 				</p>
